df5178a50d589ab095cbe9c00c3c4f5edb9554e5
ktritsh7 <ktriesh7@gmail.com>
2021-05-16 14:06:58 +0530

    Added:Lambda exp for Addition, Multiplication and Division

--- MathOperation.java
+++ MathOperation.java
@@ -1,10 +1,19 @@
 package com.bridgelabz.lambdaexpression;
 
+@FunctionalInterface
+interface IMathFunction {
+	int calculate (int a, int b);
+}
+
 public class MathOperation {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Lambda Expression");
-
+		IMathFunction add = Integer::sum;
+		System.out.println("Addition is " + add.calculate(6, 3));
+		IMathFunction multiplication = (x, y) -> x*y;
+		System.out.println("Multiplication is " + multiplication.calculate(5, 6));
+		IMathFunction division  = (int x, int y) -> x/y;
+		System.out.println("Division is " + division.calculate(10, 2));
 	}
 
 }
