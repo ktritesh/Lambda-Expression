548b7961b0711c08b544743a38441c3b97a6f068
ktritsh7 <ktriesh7@gmail.com>
2021-05-16 13:49:00 +0530

    Welcome to Lambda expression problem

---
+++ MathOperation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.lambdaexpression;
+
+public class MathOperation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Lambda Expression");
+
+	}
+
+}
